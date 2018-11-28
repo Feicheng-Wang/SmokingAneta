1dc1b72aa1875512cebfc7bf0b4d2ae99c6f827c
Feicheng Wang <44178645+pigfarm@users.noreply.github.com>
2018-11-28 17:38:19 -0500

    Add further check of valid Members

--- smoking.sql
+++ smoking.sql
@@ -1,5 +1,115 @@
 use sy169
 
+-- Find count of different MemberIds from Member Table
+select MemberId 
+into MembersRecord
+from AetnaDataWarehouse.dbo.Members
+group by MemberId
+
+Insert into MembersRecord
+select MemberId
+from AetnaDataWarehouse_2016_2017.dbo.Members
+group by MemberId
+
+select MemberId
+into MembersRecordFinal
+from MembersRecord
+group by MemberId
+
+select count(*) from MemberjoinEnrollment
+
+--Find count of different MemberIds from the join of Member and Enrollment table
+
+select A.MemberId
+into MemberjoinEnrollment
+from MembersRecordFinal A
+inner join AllEnrollmentBeginEndDate B on A.MemberId = B.MemberId
+group by A.MemberId
+
+-- Find count of different MemberIds from FactIcd Table
+drop table IcdRecordFinal
+drop table IcdRecord
+
+select MemberId 
+into IcdRecord
+from AetnaDataWarehouse.dbo.FactIcd
+group by MemberId
+
+Insert into IcdRecord
+select MemberId
+from AetnaDataWarehouse_2016_2017.dbo.FactIcd
+group by MemberId
+
+select MemberId
+into IcdRecordFinal
+from IcdRecord
+group by MemberId
+
+select count(*) from IcdRecordFinal
+
+-- Find count of different MemberIds from PharmacyClaims Table
+
+select MemberId 
+into PharmacyRecord
+from AetnaDataWarehouse.dbo.PharmacyClaims
+group by MemberId
+
+Insert into PharmacyRecord
+select MemberId
+from AetnaDataWarehouse_2016_2017.dbo.PharmacyClaims
+group by MemberId
+
+select MemberId
+into PharmacyRecordFinal
+from PharmacyRecord
+group by MemberId
+
+-- Find count of different MemberIds from  (Member join Enrollment table) join (PharmacyClaims Union FactIcd Table)
+select A.MemberId
+into TotalValidMembers
+from DrugIcdRecordFinal A
+inner join MemberjoinEnrollment B on A.MemberId = B.MemberId
+
+select count(*) from TotalValidMembers
+select count(*) from DrugIcdRecordFinal
+select count(*) from MemberjoinEnrollment
+
+
+
+
+-- Find count of different MemberIds from PharmacyClaims Union FactIcd Table
+select * 
+into DrugIcdRecord
+from PharmacyRecordFinal
+
+Insert into DrugIcdRecord
+select MemberId
+from IcdRecordFinal
+group by MemberId
+
+select MemberId
+into DrugIcdRecordFinal
+from DrugIcdRecord
+group by MemberId
+
+-- Find count of different MemberIds from MedicalClaims Table
+select MemberId 
+into MedicalRecord
+from AetnaDataWarehouse.dbo.MedicalClaims
+group by MemberId
+
+Insert into MedicalRecord
+select MemberId
+from AetnaDataWarehouse_2016_2017.dbo.MedicalClaims
+group by MemberId
+
+select MemberId
+into MedicalFinal
+from MedicalRecord
+group by MemberId
+
+
+
 -- Select all valid members from both datasets
 -- Record the number of entries as the count of total enrollment months
 --2min
@@ -13,6 +123,10 @@ FROM AetnaDataWarehouse.dbo.Enrollment
 where (EffectiveDate != '0001-01-01' )
 GROUP BY MemberId
 
+select count(*)  from tmp1AllEnrollmentEffectiveDate_2015
+
+drop table tmp1AllEnrollmentEffectiveDate_2016_2017
+
 SELECT MemberId,
        min(OriginalEffectiveDate) as OriginalEffectiveDate,
        max(EffectiveDate) AS maxEffectiveDate,
@@ -22,6 +136,34 @@ FROM AetnaDataWarehouse_2016_2017.dbo.Enrollment
 where (EffectiveDate != '0001-01-01' )
 GROUP BY MemberId
 
+/*
+with tmp as
+(
+select A.MemberId, (A.recordCount + B.recordCount) as recordCount,
+A.recordCount as recordCountA,
+B.recordcount as recordCountB
+from tmp1AllEnrollmentEffectiveDate_2015 A
+inner join tmp1AllEnrollmentEffectiveDate_2016_2017 B
+on A.MemberId = B.MemberId
+where A.recordCount >= 12
+and B.maxCancelDate > '2013-01-01'
+and A.maxCancelDate <= '2013-01-01'
+)
+select *
+from tmp
+
+with tmp as
+(
+select MemberId, sum(recordCount) as recordCount
+from tmp1AllEnrollmentEffectiveDate_2016_2017 
+where recordCount>=12
+group by MemberId
+)
+select *
+from tmp
+*/
+drop table  tmp1AllEnrollmentEffectiveDate
+
 select * 
 into tmp1AllEnrollmentEffectiveDate
 from tmp1AllEnrollmentEffectiveDate_2015
@@ -30,6 +172,7 @@ Insert into tmp1AllEnrollmentEffectiveDate
 select *
 from tmp1AllEnrollmentEffectiveDate_2016_2017
 
+drop table  tmp2AllEnrollmentEffectiveDate
 -- Merge record from both databases
 select MemberId, min(OriginalEffectiveDate) as OriginalEffectiveDate,
 max(maxEffectiveDate) AS maxEffectiveDate,
@@ -37,6 +180,10 @@ sum(recordCount) AS recordCount
 into tmp2AllEnrollmentEffectiveDate
 from tmp1AllEnrollmentEffectiveDate
 group by MemberId
+
+select count(*) from tmp2AllEnrollmentEffectiveDate
+select top 100 * from tmp2AllEnrollmentEffectiveDate
+drop table AllEnrollmentBeginEndDate
 
 select MemberId, OriginalEffectiveDate as EnrollmentStartDate,
 EOMONTH(maxEffectiveDate) as EnrollmentEndDate, recordCount
@@ -81,12 +228,19 @@ where A.Age = B.Age
 or A.Age is null
 or B.Age is null
 
+Select top 100 * from ValidEnrollmentBeginEndDateAge
+drop table ValidEnrollmentBeginEndDateAge45
+
 -- Select All those whose age >= 45 by 2018
 -- 6666688 entries
 Select *
 into ValidEnrollmentBeginEndDateAge45
 from ValidEnrollmentBeginEndDateAge
 where Age >= 45
+
+Select top 100 * from ValidEnrollmentBeginEndDateAge45
+
+drop table tmpsmokerByIcd_fw
 
 -- Select candidate Smokers by Smoking Icd
 select B.MemberId,
@@ -117,12 +271,17 @@ or B.Icd like '649.0%'
 ) and B.DateServiceStarted > '1800-01-01'
 group by B.MemberId
 
+drop table smokerByIcd_fw
+
 -- Merge two tables and record sum of each member's Icd count
 -- 1015182 entries
 select MemberId, sum(countIcd) as countIcd
 into smokerByIcd_fw
 from tmpsmokerByIcd_fw
 group by MemberId
+
+Select top 100 * from smokerByIcd_fw
+drop table tmpsmokerByDrug
 
 -- Select candidate Smokers by Drug Code
 -- 38739
@@ -154,17 +313,23 @@ GROUP BY
 P.MemberId, sm.ProprietaryName
 ORDER BY P.MemberId
 
+drop table smokerByDrug_fw
+
 --Merge two tables
 select MemberId, sum(countDrug) as countDrug
 into smokerByDrug_fw
 from tmpsmokerByDrug
 group by MemberId
 
+Select top 100 * from smokerByDrug_fw
+
 -- 25206 both have Drug and Icd records
 select count(distinct A.MemberId)
 from smokerByDrug_fw A
 inner join smokerbyIcd B
 on A.MemberId = B.MemberId
+
+drop table smokerByIcd;
 
 -- Get count from each type of valid Heavy Smokers: Icd, Drug, Icd and Drug
 Select count(distinct MemberId)
@@ -182,6 +347,8 @@ on A.MemberId = B.MemberId
 where A.countDrug>=3
 and B.countIcd>=10
 
+drop table tmpsmokerByIcdDrug_fw
+
 -- Get the table of valid heavy smokers
 SELECT
 MemberId, countIcd as CountSmokingEntries
@@ -195,6 +362,8 @@ MemberId, countDrug as CountSmokingEntries
 from smokerByDrug_fw
 where countDrug>=3
 
+drop table smokerByIcdDrug_fw
+
 -- Heavy smoker table
 -- Total entries of both Icd and Drug (may not useful)
 Select
@@ -202,6 +371,10 @@ MemberId, sum(CountSmokingEntries) as TotalSmokingEntries
 into smokerByIcdDrug_fw
 from tmpsmokerByIcdDrug_fw
 group by MemberId
+
+Select top 100 * from AetnaDataWarehouse.dbo.FactIcd
+
+drop table tmpCopdPatients_fw
 
 -- Get all heavy smokers with COPD
 -- 1min
@@ -221,6 +394,10 @@ on A.MemberId = B.MemberId
 where B.Icd like '46[0-6]%'
 or B.Icd like 'J4[0-7]%'
 
+Select top 100 * from CopdPatients
+
+drop table smokerNoCOPD_fw
+
 -- Select All heavy smokers w/o COPD
 -- 221746 entries
 select A.*
@@ -228,6 +405,8 @@ into smokerNoCOPD_fw
 FROM smokerByIcdDrug_fw A
 left join tmpCopdPatients_fw B on A.MemberId = B.MemberId
 where B.MemberId is null
+
+drop table  NoCopdCVDPatients_fw
 
 -- Select All heavy smokers w/o COPD but with CVD
 select B.MemberId, B.Icd
@@ -246,6 +425,8 @@ on A.MemberId = B.MemberId
 where B.Icd like 'I[2-5][0-9]%'
 or B.Icd like '4[1-2][0-9]%'
 
+drop table  smokerNoCOPDCVD_fw
+
 -- Select All heavy smokers w/o COPD and CVD
 -- 126247 entries
 select A.*
@@ -253,6 +434,9 @@ into smokerNoCOPDCVD_fw
 FROM smokerNoCOPD_fw A
 left join NoCopdCVDPatients_fw B on A.MemberId = B.MemberId
 where B.MemberId is null
+
+select top 100 * from cancerIcd
+drop table cancerIcd
 
 drop table NoCopdNoCVDCancerPatients_fw
 -- Select All heavy smokers w/o COPD and CVD but with cancer
@@ -272,6 +456,8 @@ on A.MemberId = B.MemberId
 inner join cancerIcd C
 on B.Icd = C.code
 
+drop table smokerNoCOPDCVDCancer_fw
+
 -- Select All heavy smokers w/o COPD, CVD and cancer
 -- 103751 entries
 select A.*
@@ -280,6 +466,10 @@ FROM smokerNoCOPDCVD_fw A
 left join NoCopdNoCVDCancerPatients_fw B on A.MemberId = B.MemberId
 where B.MemberId is null
 
+select count (*), count (distinct MemberId)
+from smokerNoCOPDCVDCancer
+
+drop table  tmpfinalsmoker_fw
 -- Get total PaidAmount from each candidate
 --1min
 select A.MemberId, sum(B.PaidAmount) as PaidAmount
@@ -300,11 +490,14 @@ where B.PaidAmount is not null
 and B.PaidAmount >= 0
 group by A.MemberId
 
+drop table finalsmoker_fw
+
 select MemberId, sum(PaidAmount) as PaidAmount
 into finalsmoker_fw
 from tmpfinalsmoker_fw
 group by MemberId
 
+drop table finalsmokerAge_fw
 -- Get Average PaidAmount by day, and total EnrollDays
 select A.*, B.Age as Age, B.Age/5 as AgeGroup,
 Datediff(day, B.EnrollmentStartDate, B.EnrollmentEndDate)+1 as enrollDays,
@@ -313,6 +506,8 @@ into finalsmokerAge_fw
 from finalsmoker_fw A
 inner join ValidEnrollmentBeginEndDateAge B
 on A.MemberId = B.MemberId
+
+drop table lowestCostSmoker_fw
 
 --Choose the lowest 10% by average 
 With SlicedData as
@@ -326,6 +521,8 @@ select *
 into lowestCostSmoker_fw
 from SlicedData 
 where Ntile = 1 
+
+select  count(*) from  finalsmokerAge_fw
 
 select AgeGroup, count(*)
 from lowestCostSmoker_fw
